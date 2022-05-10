6fa39e9d63c0847482e47f86da2fa6bbdfb959f1
Runting Han <hrt@berkeley.edu>
2022-05-10 08:13:50 +0000

    merged to main notebook and updated pytest

--- functions/tests/test_chocolate.py
+++ functions/tests/test_chocolate.py
@@ -1,3 +1,4 @@
+import minji
 import pytest
 import pandas as pd
 import numpy as np
--- test_chocolate.py
+++
@@ -1,7 +0,0 @@
-import minji
-
-data = "data/chocolate.csv"
-def test_rangescore():
-	test_range = minji.rangeScore(data['Rating'])
-	assert isinstance(test_range[0], str)
-
